6f7b2b4442939c8be76105d5b3e02d2d91b3e281
Nathaniel Haynam <natehaynam@berkeley.edu>
2022-10-05 01:40:49 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,11 +14,11 @@ class MoviesController < ApplicationController
         @ratings_to_show = Hash[@all_ratings.map {|rating| [rating, 1]}]
       end
 
-      if params[:ratings] != session[:ratings]
-      
-        session[:ratings] = @ratings_to_show
+      if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+        session[:sort] = sort
+        session[:ratings] = @selected_ratings
       end
-      @movies = Movie.where(rating: @ratings_to_show.keys)
+      @movies = Movie.where(rating: @selected_ratings.keys).order(sort)
     end
   
     def new
