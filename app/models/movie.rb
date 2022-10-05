28ccf7d5997df69e5ff7192e1ff95161c3407723
Nathaniel Haynam <natehaynam@berkeley.edu>
2022-10-05 01:04:26 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    return ['G','PG','PG-13','R']
+  end
+  def self.with_ratings(ratings_list)
+    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+    #  movies with those ratings
+    if ratings_list == nil:
+      return Movie.all 
+    return Movies params[:ratings]
+    # if ratings_list is nil, retrieve ALL movies
+    end
 end
