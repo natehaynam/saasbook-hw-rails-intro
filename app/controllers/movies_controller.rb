c28209d7b314a55c652d5f8b36e1492c6308223c
Nathaniel Haynam <natehaynam@berkeley.edu>
2022-10-05 01:29:57 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,7 @@ class MoviesController < ApplicationController
     def index
       @movies = Movie.all
       @all_ratings = Movie.all_ratings
-      @ratings_to_show = Movie.all_ratings
+      @ratings_to_show = params[:ratings] || session[:ratings] || {}
     end
   
     def new
